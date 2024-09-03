f341d79500d50eac02227d65cb8f173c2b43285f
kankantipraneeth <126898686+kankantipraneeth@users.noreply.github.com>
2024-09-03 23:18:05 +0530

    Add files via upload

---
+++ 2.3+NPM/2.3 NPM/index.js
@@ -0,0 +1,9 @@
+// import superheroes from "superheros";
+
+// // superheroes.all;
+// const randSuperHero = superheroes.random();
+// console.log("I am ${randSuperHero}!");
+import superheroes from "superheroes";
+
+const name = superheroes.random();
+console.log(`I am ${name}!`);
